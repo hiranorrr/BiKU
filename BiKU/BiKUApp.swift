f3cf2b087a96c07c53ba469e0a9622c14ea5223d
hiranorrr <hiuni_mono100@me.com>
2022-05-17 18:11:51 +0900

    Initial Commit

---
+++ BiKU/BiKUApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiKUApp.swift
+//  BiKU
+//
+//  Created by 平野瑠登 on 2022/05/17.
+//
+
+import SwiftUI
+
+@main
+struct BiKUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiKU/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BiKU
+//
+//  Created by 平野瑠登 on 2022/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
