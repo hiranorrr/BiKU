53655ff41d803e965d663ed88ab0c94134f0246a
tomi-t0mmy <tomitomit0mmyyy@gmail.com>
2022-05-31 16:16:53 +0900

    ログイン機能実装

--- BiKU/ContentView.swift
+++ BiKU/ContentView.swift
@@ -6,10 +6,14 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+
+
 
 struct LoginView: View {
     @State var inputEmail: String = ""
     @State var inputPassword: String = ""
+    @State var errorMessage: String = ""
 
     var body: some View {
         NavigationView {
@@ -31,7 +35,35 @@ struct LoginView: View {
                 .frame(height: 200)
 
                 Button(action: {
-                   print("Login処理")
+                    if(self.inputEmail == ""){
+                        self.errorMessage = "メールアドレスが入力されていません"
+                        print(self.errorMessage)
+                    }else if(self.inputPassword == ""){
+                        self.errorMessage = "パスワードが入力されていません"
+                        print(self.errorMessage)
+                    }else{
+                        Auth.auth().signIn(withEmail: inputEmail, password: inputPassword) { authResult, error in
+                            if authResult?.user != nil {
+                                // ログイン成功処理
+                                print("success")
+                            } else {
+                                // ログイン失敗処理
+                                if let error = error as NSError?, let errorCode = AuthErrorCode.Code(rawValue: error._code) {
+                                    switch errorCode {
+                                    case .invalidEmail:
+                                        self.errorMessage = "メールアドレスの形式が正しくありません"
+                                    case .userNotFound, .wrongPassword:
+                                        self.errorMessage = "メールアドレス、またはパスワードが間違っています"
+                                    case .userDisabled:
+                                        self.errorMessage = "このユーザーアカウントは無効化されています"
+                                    default:
+                                        self.errorMessage = error.domain
+                                    }
+                                }
+                                print(self.errorMessage)
+                            }
+                        }
+                    }
                 },
                 label: {
                     Text("Login")
