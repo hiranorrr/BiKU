abef54460412eb4f0fb0845f6bb00323026cf99d
tomi-t0mmy <tomitomit0mmyyy@gmail.com>
2022-06-07 03:51:25 +0900

    駐輪処理を追加

--- BiKU/ParkingList.swift
+++ BiKU/ParkingList.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 import SwiftUI
+import FirebaseFirestore
+import FirebaseAuth
  
 struct ParkingList: View {
     var parking: Parking
+    @Binding var parkings: [Parking]
+    
     var body: some View {
         VStack {
             Text(parking.name).bold().underline().font(.largeTitle)
@@ -21,18 +25,69 @@ struct ParkingList: View {
             Spacer()
             
             //　DB処理　新しい駐輪台数を書き込む
-            Button(action: {}){
+            Button(action: {
+                park()
+            }){
                 Text("Park").font(.largeTitle).foregroundColor(Color.white).padding(.horizontal).background(Color.blue)
             }
             Spacer()
         }
     }
     
-    
+    func park() {
+        let db = Firestore.firestore()
+        if let currentUser = Auth.auth().currentUser {
+            db.collection("users").document(currentUser.uid).setData([
+                "parking_id": parking.id
+            ]) { err in
+                if let err = err {
+                    // FIXME: エラーハンドリング
+                    print("Error adding document: \(err)")
+                } else {
+                    print("Document added")
+                }
+            }
+
+            let newParking = Parking(
+                id: parking.id,
+                name: parking.name,
+                amount: parking.amount + 1,
+                coordinates: parking.coordinates,
+                isParking: parking.isParking
+            )
+
+            db.collection("parking_lots").document(String(newParking.id)).setData([
+                "amount": newParking.amount,
+                "name": newParking.name,
+                "coordinates": [
+                    "latitude": newParking.coordinates.latitude,
+                    "longitude": newParking.coordinates.latitude
+                ]
+            ]) { err in
+                if let err = err {
+                    // FIXME: エラーハンドリング
+                    print("Error adding document: \(err)")
+                } else {
+                    print("Document added")
+                }
+            }
+            
+            let newParkings = parkings.map { p -> Parking in
+                if (p.id == parking.id) {
+                    return newParking
+                } else {
+                    return p
+                }
+            }
+            
+            self.parkings = newParkings
+        }
+    }
 }
  
 struct ParkingList_Previews: PreviewProvider {
     static var previews: some View {
-        ParkingList(parking: parkingData[0])
+        ParkingList(parking: parkingData[0], parkings: .constant(parkingData))
     }
 }
+
